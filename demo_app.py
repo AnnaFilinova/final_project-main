a5d671eeab6a12693243e78581cdc66671f84b66
AnnaFilinova <afilinova@nes.ru>
2021-06-03 13:51:48 +0300

    commit

--- demo_app.py
+++ demo_app.py
@@ -34,22 +34,23 @@ with st.echo(code_location='below'):
     # plt.figure(figsize=(16, 6))
     # word_freq.plot(50)
 
-    book_img = 'https://www.pinclipart.com/picdir/middle/365-3651885_book-black-and-white-png-peoplesoft-learn-peoplesoft.png'
+    #book_img = 'https://www.pinclipart.com/picdir/middle/365-3651885_book-black-and-white-png-peoplesoft-learn-peoplesoft.png'
+    book_img='https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.pinterest.com%2Fpin%2F317785317453479496%2F%3Famp_client_id%3DCLIENT_ID(_)%26mweb_unauth_id%3D%7B%7Bdefault.session%7D%7D%26from_amp_pin_page%3Dtrue&psig=AOvVaw35Ryzo_A_CPNHZTZJ7ehrm&ust=1622803794159000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICQ0rWl-_ACFQAAAAAdAAAAABAD'
     with urllib.request.urlopen(book_img) as url:
         f = BytesIO(url.read())
     img = Image.open(f)
 
     mask = np.array(img)
-    #img_color = ImageColorGenerator(mask)
-    img_color= get_single_color_func('deepskyblue')
+    img_color = ImageColorGenerator(mask)
+    #img_color= get_single_color_func('deepskyblue')
 
-    wc = WordCloud(background_color='#cdffff',
+    wc = WordCloud(background_color='white',
                    mask=mask,
                    max_font_size=2000,
                    max_words=2000,
                    random_state=42)
     wcloud = wc.generate_from_frequencies(fr)
-    fig=plt.figure(figsize=(15, 10))
+    fig=plt.figure(figsize=(16, 10))
     plt.axis('off')
     plt.imshow(wc.recolor(color_func=img_color), interpolation="bilinear")
     st.pyplot(fig)
